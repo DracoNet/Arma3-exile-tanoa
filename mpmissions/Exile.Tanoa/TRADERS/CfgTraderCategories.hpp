f22ce45a9cc67b24eccf76030a314bb543d941de
Draco Dragon <d@dracomail.net>
2017-06-30 05:00:06 +0200

    ups
    Trader FIX

--- mpmissions/Exile.Tanoa/TRADERS/CfgTraderCategories.hpp
+++ mpmissions/Exile.Tanoa/TRADERS/CfgTraderCategories.hpp
@@ -1,15 +1,15 @@
 class CfgTraderCategories 
 {
-	//#include "TRADERS\Community\TraderCategories.hpp"
-	#include "TRADERS\EXILE\TraderCategories.hpp"
-	#include "TRADERS\APEX\TraderCategories.hpp"
-	#include "TRADERS\RubberDuckGaming\TraderCategories.hpp"
+	//#include "Community\TraderCategories.hpp"
+	#include "EXILE\TraderCategories.hpp"
+	#include "APEX\TraderCategories.hpp"
+	#include "RubberDuckGaming\TraderCategories.hpp"
 	// MODS Traders
-	//#include "TRADERS\HVP\TraderCategories.hpp"
-	//#include "TRADERS\RZInfection\TraderCategories.hpp"
-	//#include "TRADERS\RHSV\TraderCategories.hpp"
-	//#include "TRADERS\RHSW\TraderCategories.hpp"
-	//#include "TRADERS\CUPV\TraderCategories.hpp"
-	//#include "TRADERS\CUPW\TraderCategories.hpp"
-	//#include "TRADERS\CUPU\TraderCategories.hpp"
+	//#include "HVP\TraderCategories.hpp"
+	//#include "RZInfection\TraderCategories.hpp"
+	//#include "RHSV\TraderCategories.hpp"
+	//#include "RHSW\TraderCategories.hpp"
+	//#include "CUPV\TraderCategories.hpp"
+	//#include "CUPW\TraderCategories.hpp"
+	//#include "CUPU\TraderCategories.hpp"
 };
